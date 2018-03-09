cd07b9fd38f64d7c2fd52d2da383f9fcbae5131d
Felipe Garcia Fogal <felipegarciafogal@MacBook-Pro-Nextel-de-Felipe.local>
2018-03-09 19:02:29 -0300

    Criando server http

--- NodeJS/noticias.js
+++ NodeJS/noticias.js
@@ -1,2 +1,8 @@
-console.log('Criando um site de noticias com NodeJS');
-console.log('Hello World!!!');
+var http = require('http');
+
+var server = http.createServer( function(req, res){
+
+    res.end("<html><body>Portal de Noticias</body></html>")
+} );
+
+server.listen(3000)
